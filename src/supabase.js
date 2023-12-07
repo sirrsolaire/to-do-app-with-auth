8ce8b92ab508d674a4f66af381d89120be3ae8df
Erkut Gürkan <111708975+sirrsolaire@users.noreply.github.com>
2023-12-07 23:09:40 +0300

    connected a live app

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://rcfwwmcmbvvtrsftdran.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjZnd3bWNtYnZ2dHJzZnRkcmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2MDg2MjUsImV4cCI6MjAxNDE4NDYyNX0.-iNqMif1u54tKaB06r7EqJDcLp8c4vmx6WLLjPQbaBA";
 
 export default createClient(supabaseUrl, supabaseAnonKey);
+
